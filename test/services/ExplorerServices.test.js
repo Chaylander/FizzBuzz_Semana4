ae8aadbe2aaf021908f591578d9f9db970c24b76
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-01 12:34:14 -0500

    Agrego pruebas de unidad para las otras funciones de ExplorerService

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -6,4 +6,15 @@ describe("Tests para explorersServices", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         expect(explorersInNode.length).toBe(1)
     })
+    test("Requerimiento 2: Filtrar por mision", () => {
+        const explorers = [{mission: "node"}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode).toEqual([{mission: "node"}]);
+    })
+    test("Requerimiento 3: Obtener githubUsername de los explorers en una mision", () => {
+        const explorer = [{mission: "node", githubUsername: "Chaylander"}];
+        const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorer, "node");
+        expect(explorersInNode).toContain("Chaylander");//Uso contain porque el valor esta dentro de un ARRAY ["Chaylander"]
+    })
+
 })
